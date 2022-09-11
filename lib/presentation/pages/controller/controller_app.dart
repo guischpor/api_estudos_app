cd14d0b2804c2299a3195ccafc984686975ad8cb
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2022-09-10 22:40:31 -0300

    up

--- lib/presentation/pages/controller/controller_app.dart
+++ lib/presentation/pages/controller/controller_app.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:api_estudos_app/core/exceptions/http_exceptions.dart';
 import 'package:api_estudos_app/core/services/client_http.dart';
 import 'package:api_estudos_app/data/model/photo.dart';
 import 'package:api_estudos_app/data/model/post.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart';
 
 class ControllerApp extends ChangeNotifier {
   final ClientHttp clientHttp;
@@ -21,18 +23,25 @@ class ControllerApp extends ChangeNotifier {
   Future<void> getPosts() async {
     _post.clear();
 
-    final response =
+    Response response =
         await clientHttp.get(url: 'https://jsonplaceholder.typicode.com/posts');
 
-    List<dynamic> data = jsonDecode(response.body);
-    _post = data.map(Post.fromJson).toList();
-    notifyListeners();
+    try {
+      List<dynamic> data = jsonDecode(response.body);
+      _post = data.map(Post.fromJson).toList();
+      notifyListeners();
+    } on HttpException catch (error) {
+      String mensagem = error.msg;
+      mensagem = 'Error';
+      debugPrint(mensagem);
+      debugPrint(error.statusCode.toString());
+    }
   }
 
   Future<void> getPhotos() async {
     _photo.clear();
 
-    final response = await clientHttp.get(
+    Response response = await clientHttp.get(
         url: 'https://jsonplaceholder.typicode.com/photos');
 
     List<dynamic> data = jsonDecode(response.body);
